7620a544d462e6e42702b074c111ffd154fa0aba
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-06-30 11:43:50 +0300

    add benchmarks

---
+++ test.py
@@ -0,0 +1,25 @@
+from base64 import b64encode
+from hashlib import sha1
+
+from mymodule import c_accept
+
+
+def py_accept(salt: str) -> str:
+    return b64encode(sha1((salt + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11').encode()).digest()).decode()
+
+
+def magic_accept(salt: str) -> str:
+    return 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='
+
+
+def test_python_code(benchmark):
+    assert benchmark(py_accept, 'dGhlIHNhbXBsZSBub25jZQ==') == 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='
+
+
+def test_optimized_code(benchmark):
+    assert benchmark(c_accept, 'dGhlIHNhbXBsZSBub25jZQ==') == 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='
+
+
+def test_magic_code(benchmark):
+    assert benchmark(magic_accept, 'dGhlIHNhbXBsZSBub25jZQ==') == 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='
+
